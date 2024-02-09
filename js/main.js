3a77d8b2b7230b0b615d769fa67d13ca6235f971
jun <juntetsu.1012@gmail.com>
2024-02-10 07:05:11 +0900

    Lenis

--- js/main.js
+++ js/main.js
@@ -3,6 +3,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
   gsap.registerPlugin(ScrollTrigger);
 
+  // Lenis
+  const lenis = new Lenis({
+    duration: 1.2
+  })
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+
+  // Integration Lenis on GSAP ScrollTrigger
+  lenis.on("scroll", ScrollTrigger.update);
+
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+  
+
   const workInfoItems = document.querySelectorAll(".work__photo-item");
   workInfoItems.forEach((item, index) => {
     item.style.zIndex = workInfoItems.length - index;
